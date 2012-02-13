e8f9b73f528f5e89659cce37cf0ff13452987cf0
Andreas Jung <info@zopyx.com>
2012-02-13 11:41:48 +0000

    updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.3.1'
+version = '0.3.1-dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
