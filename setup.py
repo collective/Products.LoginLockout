d6c59b2a27138e5a4778a203af78b90d04852ccf
Kees Hink <keeshink@gmail.com>
2015-06-08 11:59:25 +0200

    Preparing release 0.3.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.3.7.dev0'
+version = '0.3.7'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
