dec8a4b457f5ec9c3a9cf5049dfbb85eef04df9f
Kees Hink <keeshink@gmail.com>
2015-11-19 09:43:47 +0100

    Next release will be 0.4.0 (instead of 0.3.10)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.3.10.dev0'
+version = '0.4.0.dev0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
