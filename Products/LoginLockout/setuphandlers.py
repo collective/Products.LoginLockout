2051c265ef3c16041d82ef4053f5bdb679da82f4
Md Nazrul Islam <connect2nazrul@gmail.com>
2018-03-04 17:56:05 +0100

    issue #17 1. generic setup based uninstall profile is created. 2. also uninstall various method is created at setuphandler

--- Products/LoginLockout/setuphandlers.py
+++ Products/LoginLockout/setuphandlers.py
@@ -2,6 +2,7 @@ from Products.CMFCore.utils import getToolByName
 from Products.LoginLockout.plugin import PLUGIN_ID, manage_addLoginLockout
 from Products.LoginLockout.plugin import PLUGIN_TITLE
 from Products.LoginLockout.plugin import PROJECTNAME
+from Products.LoginLockout.plugin import log
 from StringIO import StringIO
 
 
@@ -106,3 +107,12 @@ def setupVarious(context):
 
     site = context.getSite()
     install(site)
+
+
+def uninstallVarious(context):
+    """"""
+    # Only run step if a flag file is present
+    if context.readDataFile('loginlockout.txt') is None:
+        return
+    log.info('LoginLockout uninstall process is starting...')
+    # may be plugin uninstall here?
