765e0580abdb3b1db5399696c1054649b76ca2c4
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-05-04 12:49:02 +0200

    update test

--- Products/LoginLockout/tests.py
+++ Products/LoginLockout/tests.py
@@ -10,37 +10,33 @@ from plone.app.testing import TEST_USER_PASSWORD
 from plone.app.testing import SITE_OWNER_NAME, SITE_OWNER_PASSWORD
 from plone.app.testing import PloneWithPackageLayer
 from plone.testing import Layer, layered
-try:
-    from plone.testing.z2 import ZSERVER_FIXTURE
-    from plone.testing.z2 import Browser
-except ImportError:
-    from plone.testing.zserver import ZSERVER_FIXTURE
-    from plone.testing.zope import Browser
+from plone.testing.zserver import ZSERVER_FIXTURE
+from plone.testing.zope import Browser
 import Products.LoginLockout
 from Products.LoginLockout.interfaces import ILoginLockoutSettings
 
 FIXTURE = PloneWithPackageLayer(
     zcml_package=Products.LoginLockout,
-    zcml_filename='configure.zcml',
-    gs_profile_id='Products.LoginLockout:default',
+    zcml_filename="configure.zcml",
+    gs_profile_id="Products.LoginLockout:default",
     name="LoginLockoutFixture",
 )
 
 INTEGRATION_TESTING = IntegrationTesting(
     bases=(FIXTURE,),
-    name='Products.LoginLockout:Integration',
+    name="Products.LoginLockout:Integration",
 )
 FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(FIXTURE, ZSERVER_FIXTURE),
-    name='Products.LoginLockout:Functional',
+    name="Products.LoginLockout:Functional",
 )
-ROBOT_TESTING = Layer(name='Products.LoginLockout:Robot')
+ROBOT_TESTING = Layer(name="Products.LoginLockout:Robot")
 
 
 def setUp(doctest):
-    layer = doctest.globs['layer']
-    app = layer['app']
-    portal = layer['portal']
+    layer = doctest.globs["layer"]
+    app = layer["app"]
+    portal = layer["portal"]
 
     def make_anon_browser(path=None):
         b = Browser(app)
@@ -51,7 +47,9 @@ def setUp(doctest):
 
     def make_admin_browser(path=None):
         b = Browser(app)
-        b.addHeader('Authorization', 'Basic {}:{}'.format(SITE_OWNER_NAME, SITE_OWNER_PASSWORD))
+        b.addHeader(
+            "Authorization", "Basic {}:{}".format(SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+        )
         if path:
             b.open(portal.absolute_url() + path)
         else:
@@ -69,13 +67,15 @@ def setUp(doctest):
         for key, value in kw.items():
             try:
                 registry = getUtility(IRegistry)
-                settings = registry.forInterface(ILoginLockoutSettings, prefix="Products.LoginLockout")
+                settings = registry.forInterface(
+                    ILoginLockoutSettings, prefix="Products.LoginLockout"
+                )
                 setattr(settings, key, value)
                 continue
             except ComponentLookupError:
                 pass
             try:
-                p_tool = getToolByName(portal, 'portal_properties')
+                p_tool = getToolByName(portal, "portal_properties")
                 p_tool.loginlockout_properties.setProperty(key, value)
                 continue
             except AttributeError:
@@ -84,41 +84,54 @@ def setUp(doctest):
 
     def get_loginlockout_settings():
         registry = getUtility(IRegistry)
-        settings = registry.forInterface(ILoginLockoutSettings, prefix="Products.LoginLockout")
+        settings = registry.forInterface(
+            ILoginLockoutSettings, prefix="Products.LoginLockout"
+        )
         return settings
 
     doctest.globs.update(locals())
-#
-#     def afterSetUp(self):
-#        # sm = getSiteManager(context=self.portal)
-#         # sm.unregisterUtility(provided=IMailHost)
-#         # sm.registerUtility(mailhost, provided=IMailHost)
-#     #
-#     # def setStatusCode(self, key, value):
-#     #     from ZPublisher import HTTPResponse
-#     #     HTTPResponse.status_codes[key.lower()] = value
-#
-    portal.error_log._ignored_exceptions = ('Unauthorized',)
+    #
+    #     def afterSetUp(self):
+    #        # sm = getSiteManager(context=self.portal)
+    #         # sm.unregisterUtility(provided=IMailHost)
+    #         # sm.registerUtility(mailhost, provided=IMailHost)
+    #     #
+    #     # def setStatusCode(self, key, value):
+    #     #     from ZPublisher import HTTPResponse
+    #     #     HTTPResponse.status_codes[key.lower()] = value
+    #
+    portal.error_log._ignored_exceptions = ("Unauthorized",)
 
     def raising(self, info):
         import traceback
+
         traceback.print_tb(info[2])
         print(info[1])
 
     from Products.SiteErrorLog.SiteErrorLog import SiteErrorLog
+
     SiteErrorLog.raising = raising
 
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTests([
-        layered(doctest.DocFileSuite(
-            '../../README.rst', setUp=setUp,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE | doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS | doctest.IGNORE_EXCEPTION_DETAIL),
-            layer=FUNCTIONAL_TESTING,
-        ),
-    ])
+    suite.addTests(
+        [
+            layered(
+                doctest.DocFileSuite(
+                    "../../README.rst",
+                    setUp=setUp,
+                    optionflags=doctest.REPORT_ONLY_FIRST_FAILURE
+                    | doctest.NORMALIZE_WHITESPACE
+                    | doctest.ELLIPSIS
+                    | doctest.IGNORE_EXCEPTION_DETAIL,
+                ),
+                layer=FUNCTIONAL_TESTING,
+            ),
+        ]
+    )
     return suite
+
 
 # from zope.testing import doctestcase
 # @doctestcase.doctestfiles('../../README.rst', optionflags=doctest.REPORT_ONLY_FIRST_FAILURE | doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
@@ -126,5 +139,5 @@ def test_suite():
 #     setUp = setUp
 
 
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
+if __name__ == "__main__":
+    unittest.main(defaultTest="test_suite")
